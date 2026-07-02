5434396a4680512e9de0641b56b7c441d7da3de8
Guilherme-coder7 <guilherme750g@gmail.com>
2026-07-02 02:22:06 -0300

    Módulo 2 - Aula 2.2

---
+++ modulo2/aula2_questao2.py
@@ -0,0 +1,12 @@
+variavel1 = "o resultado é"
+valor1 = 10
+valor2 = 3.5
+resultado = valor1 + valor2
+print(variavel1)
+print(type(variavel1))
+print(valor1)
+print(type(valor1))
+print(valor2)
+print(type(valor2))
+print(resultado)
+print(type(resultado))
